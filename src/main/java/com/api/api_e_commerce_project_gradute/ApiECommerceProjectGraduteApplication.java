799a273da6bf4118cd1a68e76e71b62c2ccc1dd4
Tra Huong <tratanhuong01@gmail.com>
2021-07-29 10:07:27 +0700

    push

---
+++ src/main/java/com/api/api_e_commerce_project_gradute/ApiECommerceProjectGraduteApplication.java
@@ -0,0 +1,13 @@
+package com.api.api_e_commerce_project_gradute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiECommerceProjectGraduteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiECommerceProjectGraduteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/api_e_commerce_project_gradute/ApiECommerceProjectGraduteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.api_e_commerce_project_gradute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiECommerceProjectGraduteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
